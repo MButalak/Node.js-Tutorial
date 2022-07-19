cb5a27e286ab5bec85b5c87f8d18b53b26e22d08
Mohammed Butalag <mohammed.butalak@gmail.com>
2022-07-20 01:49:37 +0200

    Init Commit

---
+++ SRC/app.js
@@ -0,0 +1,88 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+// Define Paths for express
+const publicDirectoryPath = path.join(__dirname, '..public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static Directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Deep Ass',
+        name: 'Mo'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Mo'
+    } )
+})
+
+app.get('/help', (req, res) => {
+    res.render('Help', {
+        title: 'What do you want?',
+        name: 'Mo'
+    } )
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an Address!'
+        })
+    }
+    
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Benghazi',
+        address: req.query.address
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+// app.get()
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Mo',
+        errorMessage: 'Page not Found'
+    })
+} )
+
+app.listen(3000, () => {
+    console.log('Server is up on port.')
+})
